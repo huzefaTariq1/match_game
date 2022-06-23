4cd491c377b5c117ea08385fed7ead1591ed3d44
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-06-23 23:46:59 +0500

    dispaly cards front and back

--- src/App.js
+++ src/App.js
@@ -2,37 +2,52 @@
 import { useState } from 'react';
 import './App.css';
 
-const cardImage=[
-  {"src":"/img/helmet-1.png"},
-  {"src":"/img/potion-1.png"},
-  {"src":"/img/ring-1.png"},
-  {"src":"/img/scrool-1.png"},
-  {"src":"/img/shield-1.png"},
-  {"src":"/img/sword-1.png"},
+const cardImage = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" },
 ]
 
 
 function App() {
 
-  const[mycards,setmycards]=useState([]);
-  const [turns,setTurns]=useState(0)
+  const [mycards, setmycards] = useState([]);
+  const [turns, setTurns] = useState(0)
 
-   
- const swapCards=()=>{
-  const cards=[...cardImage,...cardImage]
-  .sort(()=>Math.random()-0.5)
-  .map((card)=>({...card, "id":Math.random()}))
-  setmycards(cards);
-  setTurns(0);
-  
- }
 
- console.log(mycards)
- 
-      
+  const swapCards = () => {
+    const cards = [...cardImage, ...cardImage]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, "id": Math.random() }))
+    setmycards(cards);
+    setTurns(0);
+
+  }
+
+  console.log(mycards)
+
+
   return (
     <div className="App">
-       <button onClick={()=>swapCards()}>New game</button>
+      <button onClick={() => swapCards()}>New game</button>
+       
+      
+    
+     <center>
+      <div className='cr'>
+        {mycards.map((card) => {
+          return (
+            <div className='bc'  key={card.id}>
+              <img className='front' src={card.src} alt="card front"></img>
+              <img className='back' src="img/cover.png" alt="card back"></img>
+            </div>
+          )
+        })}
+      </div>
+      </center>
     </div>
   );
 }
